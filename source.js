4b233e9f0a91118c547a52be82a1cd3a25dc4bb1
aman raj singh <amanraj464@gmail.com>
2019-07-31 18:14:38 +0530

    Update source.js

--- source.js
+++ source.js
@@ -3,17 +3,13 @@ var ctx = canvas.getContext("2d");
 var radius = canvas.height / 2;
 ctx.translate(radius, radius);
 radius = radius * 0.90;
-var flag=0;
-
 myVar = setInterval(drawClock, 1000);
-
-
 function drawClock() {
   drawFace(ctx, radius);
   drawNumbers(ctx, radius);
   drawTime(ctx, radius);
 }
-
+var newVar;
 function drawFace(ctx, radius) {
   var grad;
   ctx.beginPath();
@@ -52,11 +48,10 @@ function drawNumbers(ctx, radius) {
 }
 
 function drawTime(ctx, radius){
-console.log(hour);
-  var now = new Date();
-  var hour = now.getHours();
-  var minute = now.getMinutes();
-  var second = now.getSeconds();
+  let now = new Date();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
   document.getElementById("timedisp").innerHTML = `The time is  ${hour} :: ${minute} :: ${second}`;
 
     hour=hour%12;
@@ -72,8 +67,43 @@ console.log(hour);
     drawHand(ctx, second, radius*0.9, radius*0.02);
 }
 function drawTime2(ctx, radius){
-  var hour=0,minute=0,second=0;
+  let hour=0,minute=0,second=0;
   document.getElementById("timedisp").innerHTML = `The clock is reset to  ${hour} :: ${minute} :: ${second}`;
+
+    hour=hour%12;
+    hour=(hour*Math.PI/6)+
+    (minute*Math.PI/(6*60))+
+    (second*Math.PI/(360*60));
+    drawHand(ctx, hour, radius*0.5, radius*0.07);
+    //minute
+    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+    drawHand(ctx, minute, radius*0.8, radius*0.07);
+    // second
+    second=(second*Math.PI/30);
+
+    drawHand(ctx, second, radius*0.9, radius*0.02);
+}
+
+function drawTime3(ctx, radius,seconds){
+  console.log(seconds );
+  let minute=0,hour=0;
+  if(seconds>3600){
+   hour = (seconds/3600);}
+    let remainingSeconds = seconds - (3600 * hour);
+    if(remainingSeconds>60)
+  	 minute= remainingSeconds/60;
+
+  	//7
+remainingSeconds = remainingSeconds - (minute*60);
+  let	second = remainingSeconds;
+
+console.log(hour );
+console.log(minute );
+console.log(second );
+
+
+
+  document.getElementById("timedisp").innerHTML = `The stop watch time is  ${hour} :: ${minute} :: ${second}`;
 
     hour=hour%12;
     hour=(hour*Math.PI/6)+
@@ -103,13 +133,44 @@ function live(){
   drawFace(ctx, radius);
   drawNumbers(ctx, radius);
   drawTime(ctx, radius);
+
 }
+var sec=0,hour=0,minute=0;
+function drawTimer(){
+  console.log("sec"+sec);
+  sec=(sec+1);
+  drawFace(ctx, radius);
+  drawNumbers(ctx, radius);
+  drawTime3(ctx, radius,sec);
+}
+var timerflag=0;
 function timer(){
+  if(timerflag==0){
   var btnTimer = document.getElementById("Timer");
   console.log(btnTimer);
   btnTimer.value="Stop Timer";
-  
+  console.log(btnTimer);
 
+  reset();
+  console.log(btnTimer);
+
+  console.log("here");
+  drawTimer();
+  console
+  newVar= setInterval(drawTimer, 1000);
+  timerflag=1;
+
+}
+else{
+  var btnTimer = document.getElementById("Timer");
+  btnTimer.value="start Timer";
+  console.log("here");
+
+  clearInterval(newVar);
+  timerflag=0;
+
+
+}
 }
 function reset(){
   clearInterval(myVar);
